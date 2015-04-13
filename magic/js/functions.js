6603477ce955cd01628947a10f0492fafa0a77e5
Isabel <m.isabellee@hotmail.com>
2015-04-13 12:59:39 -0400

    This sucks! DX

--- magic/js/functions.js
+++ magic/js/functions.js
@@ -1,14 +1,3 @@
 $(document).ready(function(){
   $('#menu').load("navbar.html");
 });
-
-function sendEmail() {
-  var name = $("#contact-name").val();
-  var email = $("#contact-email").val();
-  var message = $("#contact-message").val();
-  var link = "mailto:m.isabellee@gmail.com"
-             + "?subject=" + encodeURIComponent("Contact Form Response")
-             + "&body=" + encodeURIComponent(message+"\n\nFrom:\n"+name+"\n"+email);
-  window.location.href = link;
-  console.log(link);
-}
