66f0f3c51067937ca5a6226a7d1a455f0c11bb8e
Isabel <m.isabellee@hotmail.com>
2015-04-13 09:18:32 -0400

    Eh...trying again?

--- magic/js/functions.js
+++ magic/js/functions.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
   $('#menu').load("navbar.html");
 });
+
+function sendEmail() {
+  var name = $("#contact-name").val();
+  var email = $("#contact-email").val();
+  var message = $("#contact-message").val();
+  var link = "mailto:CSmagicMentors@gmail.com"
+             + "?subject=" + encodeURIComponent("Contact Form Response")
+             + "&body=" + encodeURIComponent(message+"\n\nFrom:\n"+name+"\n"+email);
+  window.location.href = link;
+}
